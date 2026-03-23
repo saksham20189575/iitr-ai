12a0a3167b87bc77afd33d62b0353d3755fec52b
Saksham Arora <saksham_arora@intuit.com>
2026-03-23 21:57:45 +0530

    updated program

--- session1/comparisonOperations.py
+++ session1/comparisonOperations.py
@@ -10,3 +10,11 @@ print(score >= 85)  # True
 is_adult = age >= 18
 print(is_adult)     # True
 print(type(is_adult))  # <class 'bool'>
+
+print(5 == 5.1) # False
+
+print(5 == 5.0) # True
+
+print(5 == 5.00000000000000000000000001) # True
+
+print(5 == 5.0000000001) # False
